35be2a1498109ce50b49dee75fed611cdd782348
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-07-09 11:25:22 -0400

    Implemented use of app.js file for middleware and basic route

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
 require("dotenv").config();
+const express = require("express");
+const app = express();
+// const cors = require("cors");
+const helmet = require("helmet");
+
+// app.use(cors)
+app.use(express.json());
+app.use(helmet());
+
+app.get("/", (req, res) => {
+  res.send("JOULE Active");
+});
+
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,6 @@
 require("dotenv").config();
-
-const express = require("express");
-const app = express();
+const app = require("./app");
 const { PORT } = process.env;
-
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
 
 app.listen(PORT, () => {
   console.log(`App is up and running on PORT: ${PORT}`);
