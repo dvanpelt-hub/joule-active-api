f7b91d22a82fded8fc71d0186315338f4813abcd
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-07-09 11:18:34 -0400

    Updated PORT

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const port = 3020;
+const { PORT } = process.env;
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.listen(port, () => {
-  console.log(`App is up and running on PORT: ${port}`);
+app.listen(PORT, () => {
+  console.log(`App is up and running on PORT: ${PORT}`);
 });
