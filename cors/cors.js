c7d22edf1b1c39e62bcb9417fc1062b9874ffc3c
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-07-20 19:40:35 -0400

    Adjusted cors policy

--- cors/cors.js
+++ cors/cors.js
@@ -2,9 +2,9 @@ const cors = require("cors");
 
 const CLIENT_ORIGIN = {
   origin: [
-    "http://localhost:3000",
-    "http://localhost:3050/api/v1",
-    "http://localhost:3050",
+    "http://localhost:3000/",
+    "http://localhost:3050/api/v1/",
+    "http://localhost:3050/",
     "https://mysterious-basin-15345.herokuapp.com/api/v1/mailingList",
     "https://mysterious-basin-15345.herokuapp.com",
   ],
