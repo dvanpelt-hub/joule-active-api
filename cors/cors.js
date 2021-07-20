6ec0f1c03694d63a532a11de25478d42ee735795
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-07-20 19:51:06 -0400

    Update for cors

--- cors/cors.js
+++ cors/cors.js
@@ -6,7 +6,7 @@ const CLIENT_ORIGIN = {
     "http://localhost:3050/api/v1/",
     "http://localhost:3050/",
     "https://mysterious-basin-15345.herokuapp.com/api/v1/mailingList",
-    "https://mysterious-basin-15345.herokuapp.com",
+    "https://mysterious-basin-15345.herokuapp.com/",
   ],
   optionsSuccessStatus: 200,
 };
