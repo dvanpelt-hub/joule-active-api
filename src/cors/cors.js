7df135a2089eada8016e1b23486cb7503b555d6f
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-07-21 12:33:34 -0400

    Heroku

--- src/cors/cors.js
+++ src/cors/cors.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://joule-active-client.vercel.app"],
+  origin: ["http://localhost:3000/", "https://joule-active-client.vercel.app"],
   optionsSuccessStatus: 200,
 };
 
