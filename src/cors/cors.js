03319b084102d590dae42911d3bf162e8f993555
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-07-21 10:47:05 -0400

    Updated DATABASE_URL

--- src/cors/cors.js
+++ src/cors/cors.js
@@ -1,13 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "http://localhost:3000/",
-  // "http://localhost:3000/api/v1/",
-  // "http://localhost:3050/api/v1/",
-  // "http://localhost:3050/",
-  // "https://mysterious-basin-15345.herokuapp.com/api/v1/mailingList",
-  // "https://mysterious-basin-15345.herokuapp.com/",
-  // "https://joule-active-client.vercel.app",
+  origin: ["http://localhost:3000", "https://joule-active-client.vercel.app"],
   optionsSuccessStatus: 200,
 };
 
