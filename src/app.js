a5066d2be2de048e38c6e6905fffbcad4e41393e
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-07-21 13:16:59 -0400

    Update cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const mailingListRouter = require("./routes/mailing-list-router");
 const corsMiddleware = require("./cors/cors");
 const helmet = require("helmet");
 const app = express();
 
-app.use(corsMiddleware);
+app.use(cors(corsMiddleware));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
