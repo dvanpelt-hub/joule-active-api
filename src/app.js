d940a8f712f2d6acb7edcf1affa25a85b486f76d
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-07-21 12:11:23 -0400

    Updated cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const mailingListRouter = require("./routes/mailing-list-router");
-const app = express();
-const corsMiddleware = require("./cors/cors");
+const cors = require("cors");
 const helmet = require("helmet");
+const app = express();
 
-app.use(corsMiddleware);
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 
