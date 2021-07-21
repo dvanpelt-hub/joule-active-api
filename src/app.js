6d584542d1c0ab5dcd786dd8a9c06584d5907763
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-07-21 12:30:42 -0400

    Heroku

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const mailingListRouter = require("./routes/mailing-list-router");
-const cors = require("cors");
+const corsMiddleware = require("./cors/cors");
 const helmet = require("helmet");
 const app = express();
 
-app.use(cors);
+app.use(corsMiddleware);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  next();
+});
 app.use(express.json());
 app.use(helmet());
 
