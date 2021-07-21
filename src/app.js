abf7d82fe32c4a7c2b95bfe3a9a106c2afad8c05
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-07-21 13:27:46 -0400

    Update heroku

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mailingListRouter = require("./routes/mailing-list-router");
-const corsMiddleware = require("./cors/cors");
+const { CLIENT_ORIGIN } = require("./cors/cors");
 const helmet = require("helmet");
 const app = express();
 
-app.use(cors(corsMiddleware));
+app.use(cors(CLIENT_ORIGIN));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
